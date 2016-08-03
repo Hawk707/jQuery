c6782395d868117bab1f573192468df11c46c030
Hawk707 <h.alzeini3@gmail.com>
2016-08-03 17:19:39 +0800

    finished DOM manipulation

--- tutorials/tutorialspoint.com/helloWorld/js/main.js
+++ tutorials/tutorialspoint.com/helloWorld/js/main.js
@@ -29,4 +29,19 @@ $(document).ready(function(){
 	$("#myul").find("li").eq(3).css({"color":"red", "background-color":"green"});
 	$("#div1").width(100);
 	$("#div1").css("background-color", "blue");
+	
+	//DOM Manipulation starts here:
+	$("#division").click(function(){
+		var content = $(this).html();
+		$("#result").text(content);
+		$(this).replaceWith("<h1>jQuery is Great</h1>");
+	});
+	
+	$(".divM").click(function(){
+		$(this).remove();
+	});
+	
+	$(".divD").click(function(){
+		$(this).before('<div class="divD"></div>');
+	});
 });
