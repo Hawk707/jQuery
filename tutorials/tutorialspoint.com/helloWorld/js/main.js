1353083d59af8f8eb48950be08124ef0b74b279f
Hawk707 <h.alzeini3@gmail.com>
2016-08-10 15:40:32 +0800

    finished ajax

--- tutorials/tutorialspoint.com/helloWorld/js/main.js
+++ tutorials/tutorialspoint.com/helloWorld/js/main.js
@@ -49,4 +49,25 @@ $(document).ready(function(){
 	$(".divE").bind('click', function(event){
 		alert('Hi there!');
 	});
+	
+	//Ajax
+	$("#driver").click(function(event){
+		$('#stage').load('/jquery/result.html');
+	});
+	
+	$("#driver1").click(function(event){
+				
+               $.getJSON('/jquery/result.json', function(jd) {
+                  $('#stage1').html('<p> Name: ' + jd.name + '</p>');
+                  $('#stage1').append('<p>Age : ' + jd.age+ '</p>');
+                  $('#stage1').append('<p> Sex: ' + jd.sex+ '</p>');
+               });
+					
+            });
+	$("#driver2").click(function(event){
+               var name = $("#name").val();
+               $("#stage2").load('/jquery/result.php', {"name":name} );
+            });
+	
+	
 });
